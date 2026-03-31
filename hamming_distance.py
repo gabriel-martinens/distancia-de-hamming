8126c8541ded07832f668364be2a3dce214b3683
jokecake <jokecake109@gmail.com>
2026-03-30 22:33:35 -0300

    Add type hints to hamming_distance function

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,8 +1,8 @@
-def hamming_distance(text1, text2):
+def hamming_distance(text1: bytes, text2) -> int:
   distancia = 0
   for x,y in zip(text1, text2):
     xor = x ^ y
     distancia += bin(xor).count("1")
   return distancia
 
-print(hamming_distance("git", "hub")
+print(hamming_distance(b"git", b"hub"))
